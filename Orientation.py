6447f51e4fad317901fbb0c3cc36c3f640d670cc
Tyler Allen <37377528+tylera277@users.noreply.github.com>
2024-05-22 21:49:18 -0400

    Add files via upload

--- Orientation.py
+++ Orientation.py
@@ -1,4 +1,7 @@
 
+import serial
+import numpy as np
+import time
 
 class Orientation:
     """
@@ -7,6 +10,40 @@ class Orientation:
     (Mainly interacting with the "Adafruit BNO055 Absolute Orientation Sensor" module
     that I will be using in order to get this information from/about the user)
     """
-    def __init__(self):
-        pass
+    def __init__(self, picoPortString):
+        self.port_of_pico_string = picoPortString
+        self.ser = serial.Serial("{}".format(self.port_of_pico_string), 115200, 8, "N", 1, timeout=.001)
+        self.temp_storage = [0,0,0]
 
+
+
+
+    def clean_up_orientation_vector(self):
+        # I will clean up this disgrace of a parser eventually with something that looks more
+        # professional
+
+        chunks = self.temp_storage
+        chunks = chunks.replace(",", "")
+        chunks = chunks.replace("(", "")
+        chunks = chunks.replace(")", "")
+
+        return chunks.split()
+
+
+
+    def read(self):
+        start = time.time()
+        line = self.ser.readline()
+        #print(line.decode("utf-8"))
+        if line:
+            #file.write(line.decode("utf-8"))
+            self.temp_storage = line.decode("utf-8")
+
+        print("Execution time=", time.time()-start)
+        #print("BEEP:", self.temp_storage.split())
+        return self.clean_up_orientation_vector()
+
+    
+    def getUserOrientation(self):
+        orientation = np.array((self.get_phi(), self.get_theta(), self.get_psi()))
+        return orientation
--- Sound.py
+++ Sound.py
@@ -1,4 +1,12 @@
 
+import math
+import pyaudio
+import wave
+import time
+
+import numpy as np
+
+from SoundSource import SoundSource
 
 
 class Sound:
@@ -13,19 +21,97 @@ class Sound:
         for the sound not to be laggy and cause a poor/unpleasant experience.
     """
 
-    def __init__(self):
+    def __init__(self, buffer_size, outputDevice):
+
+        self.buffer_size = buffer_size
+        self.outputDevice = outputDevice
 
         sound_source_1 = None
         sound_source_2 = None
         sound_source_3 = None
 
+        self.SPEED_OF_SOUND = 343
+        self.mix = np.zeros(2*self.buffer_size, dtype=np.int16)
+        self.data_out = np.zeros(2*self.buffer_size, dtype=np.int16)
+        self.left_channel_current_place = 0
+        self.right_channel_current_place = 0
+        self.amp_left = 1
+        self.amp_right = 1
+        self.angle = 0
+        
+        self.wf = wave.open("Sound_Sources/flowing_stream.wav", "rb")
+        self.pa = pyaudio.PyAudio()
+        
+        self.stream_out = self.pa.open(
+            rate=self.wf.getframerate(),
+            channels=2,
+            format=self.pa.get_format_from_width(self.wf.getsampwidth()),
+            output=True,
+            output_device_index=3,
+            frames_per_buffer=self.buffer_size
+        )
+        
+
+        # Read the entire .wav file into memory once before the stream
+        whole_audio_data_source1 = self.wf.readframes(self.wf.getnframes())
+        print("Length: ", len(whole_audio_data_source1))
+
+        # Convert the whole file into a workable numpy array
+        whole_audio_data = np.frombuffer(whole_audio_data_source1, dtype=np.int16)
+
+        # Splitting the interleaved file into each channel for (hopefully)
+        # better independent control of each
+        self.whole_audio_data_left = whole_audio_data[0::2]
+        self.whole_audio_data_right = whole_audio_data[1::2]
+
     def create_sound_source(self, sound_source_number, file_path, source_position_vector):
-        pass
+
+        # Only doing one sound at the moment
+        if(sound_source_number == 1):
+            sound_source_1 = SoundSource(file_path, source_position_vector)
     
 
-    def compute(self, users_orientation_vector, users_position_vector):
-        pass
+    def ITD(self, distance_from_listener, angle_in_rad):
+        time = abs(distance_from_listener/self.SPEED_OF_SOUND * (angle_in_rad + math.sin(angle_in_rad)))
+
+        return time
+
+    def ILD(self, angle_in_rad):
+        return math.cos(angle_in_rad)
 
 
-    def output_sound_to_user(self):
-        pass
+    def compute(self, users_orientation_vector):
+        start_time = time.time()
+        first_time = True
+        print("Meow:", users_orientation_vector[0])
+
+        position_of_sound_angle = float(users_orientation_vector[0])
+
+        frame_delay = int(self.ITD(1, position_of_sound_angle))
+        amp_diff = self.ILD(position_of_sound_angle)
+
+        if(first_time):
+            delay = np.zeros((frame_delay))
+            if (position_of_sound_angle >= 0) and (position_of_sound_angle < np.pi):
+                self.whole_audio_data_left = np.insert(self.whole_audio_data_left, 0, delay, axis=0)
+                self.amp_left = (amp_diff * self.amp_right) + 0.1
+            else:
+                self.whole_audio_data_right = np.insert(self.whole_audio_data_right, 0, delay, axis=0)
+                self.amp_right = (amp_diff * self.amp_left) + 0.1
+
+
+        # This needs to be at the end of the execution so it sends the right 
+        # format to be played out on the speakers
+        self.mix[0::2] = self.amp_left * self.whole_audio_data_left[self.left_channel_current_place:self.left_channel_current_place + self.buffer_size]
+        self.mix[1::2] = self.amp_right * self.whole_audio_data_right[self.right_channel_current_place:self.right_channel_current_place + self.buffer_size]
+        data_out = np.chararray.tobytes(self.mix.astype(np.int16))
+        self.stream_out.write(data_out)
+
+        # Moving the markers ahead by a full buffer size
+        self.left_channel_current_place += self.buffer_size
+        self.right_channel_current_place += self.buffer_size
+
+
+
+    def output_sound_to_user(self, orientationVector):
+        self.compute(orientationVector)        
